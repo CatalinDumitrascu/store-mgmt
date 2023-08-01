9e80c50e53c08b85bd44e9efb62edf3aaeaa666f
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-01 22:20:36 +0300

    Controller annotation and info endpoint

--- store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
@@ -1,4 +1,17 @@
 package com.ingexample.storemgmt.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @GetMapping("/info")
+    public ResponseEntity<?> info() {
+        return new ResponseEntity<>("Store Products API is up and running!", HttpStatus.OK);
+    }
 }
