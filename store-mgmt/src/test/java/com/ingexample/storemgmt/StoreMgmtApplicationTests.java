1e4ca28e89260978555eba9806c64982e85a11d7
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-01 21:56:59 +0300

    Initial Commit
    Project generated using spring initializr

---
+++ store-mgmt/src/test/java/com/ingexample/storemgmt/StoreMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingexample.storemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
