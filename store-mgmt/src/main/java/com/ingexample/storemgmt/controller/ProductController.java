f34880f82f50ce6d4f508508089646230fab2f48
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-01 22:42:33 +0300

    End2End implementation for retrieving all products

--- store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.ingexample.storemgmt.controller;
 
+import com.ingexample.storemgmt.entity.Product;
+import com.ingexample.storemgmt.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/info")
     public ResponseEntity<?> info() {
         return new ResponseEntity<>("Store Products API is up and running!", HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getProducts() {
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
     }
 }
--- store-mgmt/src/main/java/com/ingexample/storemgmt/service/ProductService.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.ingexample.storemgmt.service;
 
+import com.ingexample.storemgmt.entity.Product;
+import com.ingexample.storemgmt.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
 }
