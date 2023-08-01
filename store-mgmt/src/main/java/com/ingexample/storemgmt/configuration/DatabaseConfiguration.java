54c926a628d7364aea27d25a14b491a94edfc950
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-02 00:56:24 +0300

    Logging for controller and database config

--- store-mgmt/src/main/java/com/ingexample/storemgmt/configuration/DatabaseConfiguration.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/configuration/DatabaseConfiguration.java
@@ -2,6 +2,8 @@ package com.ingexample.storemgmt.configuration;
 
 import com.ingexample.storemgmt.entity.Product;
 import com.ingexample.storemgmt.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +12,15 @@ import java.time.LocalDate;
 
 @Configuration
 public class DatabaseConfiguration {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
     @Bean
     CommandLineRunner initDatabase(ProductRepository repository) {
 
         return args -> {
-            repository.save(new Product("Telefon", 500d, 42, LocalDate.of(2026, 10, 10)));
-            repository.save(new Product("Casa", 20000d, 20, LocalDate.of(2056, 10, 10)));
+            log.info("Loading entity into db: " + repository.save(new Product("Telefon", 500d, 42, LocalDate.of(2026, 10, 10))));
+            log.info("Loading entity into db: " + repository.save(new Product("Casa", 20000d, 20, LocalDate.of(2056, 10, 10))));
         };
     }
 }
--- store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
@@ -7,13 +7,17 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     private ProductService productService;
 
@@ -23,20 +27,24 @@ public class ProductController {
 
     @GetMapping("/info")
     public ResponseEntity<?> info() {
+        log.info(LocalDateTime.now() + "Info endpoint called");
         return new ResponseEntity<>("Store Products API is up and running!", HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<?> getProducts() {
+        log.info(LocalDateTime.now() + "Get all endpoint called");
         ResponseEntity<?> response = null;
         try {
             List<Product> productList = productService.getProducts();
+            log.info(LocalDateTime.now() + " Successfully retrieved product list: " + productList);
             if(productList != null && productList.size() > 0) {
                 response = new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
             } else {
                 response = new ResponseEntity<>(new CustomErrorResponse("404", "No items found."), HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
+            log.warn(LocalDateTime.now() + " Exception encountered while trying to retrieve product list. Exception: " + e.getMessage());
             response = new ResponseEntity<>(new CustomErrorResponse("400", e.getMessage()), HttpStatus.BAD_REQUEST);
         }
 
@@ -45,15 +53,18 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getProductById(@PathVariable Long id) {
+        log.info(LocalDateTime.now() + "Get product by id endpoint called");
         ResponseEntity<?> response = null;
         try {
             Product product = productService.getProductById(id);
+            log.info(LocalDateTime.now() + " Successfully retrieved product with id: " + id + "; Product: \n" + product);
             if(product != null) {
                 response = new ResponseEntity<>(product, HttpStatus.OK);
             } else {
                 response = new ResponseEntity<>(new CustomErrorResponse("404", "No product found for id = " + id), HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
+            log.warn(LocalDateTime.now() + " Exception encountered while trying to retrieve product with id: " + id + "; Exception: " + e.getMessage());
             response = new ResponseEntity<>(new CustomErrorResponse("400", e.getMessage()), HttpStatus.BAD_REQUEST);
         }
 
@@ -62,12 +73,15 @@ public class ProductController {
 
     @PostMapping()
     public ResponseEntity<?> addProduct(@Valid @RequestBody Product product) {
+        log.info(LocalDateTime.now() + " Post endpoint for creating product called");
         ResponseEntity<?> response = null;
 
         try {
             productService.addProduct(product);
+            log.info(LocalDateTime.now() + "; Successfully created entry for product:\n" + product);
             response = new ResponseEntity<>("Product added successfully!", HttpStatus.CREATED);
         } catch (Exception e) {
+            log.warn(LocalDateTime.now() + " Exception encountered while trying to create product. Exception: " + e.getMessage());
             response = new ResponseEntity<>(new CustomErrorResponse("400", e.getMessage()), HttpStatus.BAD_REQUEST);
         }
 
@@ -77,17 +91,20 @@ public class ProductController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updateProduct(@PathVariable Long id,
                                            @Valid @RequestBody Product product) {
+        log.info(LocalDateTime.now() + " Put endpoint for updating product called");
         ResponseEntity<?> response = null;
 
         try {
             Product updatedProduct = productService.updateProduct(id, product);
             if(updatedProduct != null) {
+                log.info(LocalDateTime.now() + " Successfully retrieved product with id: " + id + "; Product: \n" + product);
                 response = new ResponseEntity<>(updatedProduct, HttpStatus.OK);
             } else {
                 response = new ResponseEntity<>(new CustomErrorResponse("404", "No product found to update for id = " + id),
                         HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
+            log.warn(LocalDateTime.now() + " Exception encountered while trying to update product. Exception: " + e.getMessage());
             response = new ResponseEntity<>(new CustomErrorResponse("400", e.getMessage()), HttpStatus.BAD_REQUEST);
         }
 
