01b0fb0afd9fab8090cfc8cde2164d8e4a152e8f
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-01 23:04:15 +0300

    Custom response message for get all mapping

--- store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.ingexample.storemgmt.controller;
 
 import com.ingexample.storemgmt.entity.Product;
+import com.ingexample.storemgmt.helpers.CustomErrorResponse;
 import com.ingexample.storemgmt.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -25,6 +28,19 @@ public class ProductController {
 
     @GetMapping()
     public ResponseEntity<?> getProducts() {
-        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+        ResponseEntity<?> response = null;
+        List<Product> productList = null;
+        try {
+            productList = productService.getProducts();
+            if(productList != null && productList.size() > 0) {
+                response = new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+            } else {
+                response = new ResponseEntity<>(new CustomErrorResponse("404", "No items found."), HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            response = new ResponseEntity<>(new CustomErrorResponse("400", e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+
+        return response;
     }
 }
