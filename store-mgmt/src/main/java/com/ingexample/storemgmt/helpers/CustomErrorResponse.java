3e4c1c0ffdef258f3cc907b3f8e1453e4a23d7a1
Catalin Dumitrascu <cata_nicolae97@yahoo.com>
2023-08-03 01:54:33 +0300

    Update timestamp JsonFormat annotation

--- store-mgmt/src/main/java/com/ingexample/storemgmt/helpers/CustomErrorResponse.java
+++ store-mgmt/src/main/java/com/ingexample/storemgmt/helpers/CustomErrorResponse.java
@@ -2,14 +2,15 @@ package com.ingexample.storemgmt.helpers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class CustomErrorResponse {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime timestamp;
+
     private String status;
+
     private String message;
 
 
